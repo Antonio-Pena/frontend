f8808ae63bf818fe9d718bdbcef20b57b9dff896
Antonio Pena <sirantoniog@gmail.com>
2022-12-08 15:38:43 -0500

    feat(frontend): :sparkles: service to manage parameters and moduleParameters

--- services/analysisModules.ts
+++ services/analysisModules.ts
@@ -1,27 +1,86 @@
 import axios from "axios";
-import { IAnalysisModule } from "../src/types/AnalisisModule";
+import {
+  IAnalysisModule,
+  TModuleParameter,
+  TParameter,
+} from "../src/types/AnalisisModule";
 
-const baseUrl = "http://localhost:3001/analysisModules";
+const baseUrl = "http://localhost:3001";
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
+//Modules
+const getAllModules = () => {
+  const request = axios.get(`${baseUrl}/analysisModules`);
   return request.then((res) => res.data);
 };
 
-const create = (newAnalysisModule: IAnalysisModule) => {
-  const request = axios.post(baseUrl, newAnalysisModule);
+const getModule = (id: string) => {
+  const request = axios.get(`${baseUrl}/analysisModules/${id}`);
   return request.then((res) => res.data);
 };
 
-// const erase = (moduleId) => {
-//   return axios.delete(`${baseUrl}/${id}`);
-// };
+const createModule = (newAnalysisModule: IAnalysisModule) => {
+  const request = axios.post(`${baseUrl}/analysisModules`, newAnalysisModule);
+  return request.then((res) => res.data);
+};
 
-// const update = (id, newAnalysisModule) => {
-//   const request = axios.put(`${baseUrl}/${id}`, newPerson);
-//   return request.then((res) => res.data);
-// };
+const eraseModule = (id: string) => {
+  return axios.delete(`${baseUrl}/analisisModules/${id}`);
+};
 
-const analysisModulesService = { getAll, create };
+const updateModule = (id: string, AnalysisModule: IAnalysisModule) => {
+  const request = axios.put(`${baseUrl}/analisisModules/${id}`, AnalysisModule);
+  return request.then((res) => res.data);
+};
+
+//Parameters
+const getParameter = (id: string) => {
+  const request = axios.get(`${baseUrl}/parameters/${id}`);
+  return request.then((res) => res.data);
+};
+
+const createParameter = (newParameter: TParameter) => {
+  const request = axios.post(`${baseUrl}/parameters`, newParameter);
+  return request.then((res) => res.data);
+};
+
+//moduleParameters
+const getAllModuleParameters = () => {
+  const request = axios.get(`${baseUrl}/moduleParameters`);
+  return request.then((res) => res.data);
+};
+
+const createModuleParameter = (newModuleParameter: TModuleParameter) => {
+  const request = axios.post(`${baseUrl}/moduleParameters`, newModuleParameter);
+  return request.then((res) => res.data);
+};
+
+const eraseModuleParameter = (moduleId: string) => {
+  return axios.delete(`${baseUrl}/analisisModules?moduleId=${moduleId}`);
+};
+
+const updateModuleParameter = (
+  moduleId: string,
+  moduleParameter: TModuleParameter
+) => {
+  const request = axios.put(
+    `${baseUrl}/analisisModules?moduleId=${moduleId}`,
+    moduleParameter
+  );
+  return request.then((res) => res.data);
+};
+
+const analysisModulesService = {
+  getAllModules,
+  getModule,
+  createModule,
+  eraseModule,
+  updateModule,
+  getParameter,
+  createParameter,
+  getAllModuleParameters,
+  createModuleParameter,
+  eraseModuleParameter,
+  updateModuleParameter,
+};
 
 export default analysisModulesService;
