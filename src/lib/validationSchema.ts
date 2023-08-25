a3c9b66ecf47bf3e3259597bf986d0f0d3cae6fd
Antonio Peña <sirantoniog@gmail.com>
2023-08-24 20:40:09 -0500

    feat(frontend): update validation schema

--- src/lib/validationSchema.ts
+++ src/lib/validationSchema.ts
@@ -1,11 +1,11 @@
 import * as Yup from "yup";
 
 const name = Yup.string()
-  .min(5, "Module name must be at least 5 characters long")
-  .max(30, "Module name is too long")
-  .required("Module name is required");
+  .min(5, "Name must be at least 5 characters long")
+  .max(30, "Name is too long")
+  .required("Name is required");
 
-const version = Yup.string().required("Module version is required");
+const version = Yup.string().required("Version is required");
 // const parameter = Yup.string().required("Parameter name is required");
 
 export const analysisModulesValidationSchema = Yup.object({
